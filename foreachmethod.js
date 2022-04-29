baa383b21b7ffd2f32982da828833167c6bd84a8
Dabble362 <75595585+Dabble362@users.noreply.github.com>
2022-04-28 21:11:18 -0400

    add forEachMethod

---
+++ foreachmethod.js
@@ -0,0 +1,9 @@
+const fruits = ["mango", "papaya", "pineapple", "apple"];
+
+// Iterate over fruits below
+
+function iWantEat(food) {
+  console.log(`I want to eat a ${food}.`);
+}
+
+fruits.forEach(iWantEat);
