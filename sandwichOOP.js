3621f3931a181d257886dfaa3f31a40cf8eedcc4
Dabble362 <75595585+Dabble362@users.noreply.github.com>
2022-05-03 19:34:28 -0400

    add sandichOOP.js

---
+++ sandwichOOP.js
@@ -0,0 +1,9 @@
+let sandwich = {
+    size: 'large',
+    price: 11.95,
+    toppings: ['lettuce', 'tomato', 'mayo', 'dijon', 'cheddar'],
+    orderSandwich: function () {
+      return ` Your ${this.size} sandwich with ${this.toppings[0]},  ${this.toppings[1]},  ${this.toppings[2]},  ${this.toppings[3}, and  ${this.toppings[4]} comes to $${this.price}.`
+    }
+    
+  }
