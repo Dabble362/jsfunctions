cbd44fe62021c401ec2486e38bfb70b15b31e24c
Dabble362 <75595585+Dabble362@users.noreply.github.com>
2022-05-03 19:35:09 -0400

    fix sandwichOOP.js

--- sandwichOOP.js
+++ sandwichOOP.js
@@ -1,9 +1,8 @@
 let sandwich = {
-    size: 'large',
-    price: 11.95,
-    toppings: ['lettuce', 'tomato', 'mayo', 'dijon', 'cheddar'],
-    orderSandwich: function () {
-      return ` Your ${this.size} sandwich with ${this.toppings[0]},  ${this.toppings[1]},  ${this.toppings[2]},  ${this.toppings[3}, and  ${this.toppings[4]} comes to $${this.price}.`
-    }
-    
-  }
+  size: "large",
+  price: 11.95,
+  toppings: ["lettuce", "tomato", "mayo", "dijon", "cheddar"],
+  orderSandwich: function () {
+    return ` Your ${this.size} sandwich with ${this.toppings[0]},  ${this.toppings[1]},  ${this.toppings[2]},  ${this.toppings[3]}, and  ${this.toppings[4]} comes to $${this.price}.`;
+  },
+};
